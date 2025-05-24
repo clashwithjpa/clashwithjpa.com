798fe8602b91ccaf374389c4ccff1202042d346f
swayam25 <178068815+swayam25@users.noreply.github.com>
2025-05-24 10:58:53 +0530

    Update Sidebar

--- src/lib/components/admin/Bar.svelte
+++ src/lib/components/admin/Bar.svelte
@@ -51,15 +51,12 @@
     import { page } from "$app/state";
     import type { UserData } from "$lib/auth/user";
     import * as Avatar from "$lib/components/admin/ui/avatar";
-    import * as Breadcrumb from "$lib/components/admin/ui/breadcrumb";
     import { Button } from "$lib/components/admin/ui/button";
-    import { Separator } from "$lib/components/admin/ui/separator";
     import * as Sidebar from "$lib/components/admin/ui/sidebar";
     import * as Tooltip from "$lib/components/admin/ui/tooltip";
-    import type { Snippet } from "svelte";
     import LucideLogOut from "~icons/lucide/log-out";
 
-    let { user, children }: { user: UserData; children: Snippet } = $props();
+    let { user }: { user: UserData } = $props();
 
     async function logout() {
         await goto("/");
@@ -68,87 +65,56 @@
     }
 </script>
 
-<Sidebar.Provider>
-    <Sidebar.Root variant="floating">
-        <Sidebar.Content>
-            <Sidebar.Group>
-                <Sidebar.Menu>
-                    {#each data as item (item.title)}
-                        <Sidebar.MenuItem>
-                            <Sidebar.SidebarMenuButton isActive={item.url === page.url.pathname}>
-                                {#snippet child({ props })}
-                                    <a href={item.url} {...props}>
-                                        <item.icon />
-                                        <span>{item.title}</span>
-                                    </a>
-                                {/snippet}
-                            </Sidebar.SidebarMenuButton>
-                        </Sidebar.MenuItem>
-                    {/each}
-                </Sidebar.Menu>
-            </Sidebar.Group>
-        </Sidebar.Content>
-        <Sidebar.Footer>
+<Sidebar.Root variant="floating">
+    <Sidebar.Content>
+        <Sidebar.Group>
             <Sidebar.Menu>
-                <Sidebar.MenuItem>
-                    <div class="flex items-center justify-between gap-2 px-1 py-1.5 text-left text-sm">
-                        <div class="flex items-center gap-2">
-                            <Avatar.Root class="size-8 rounded-lg">
-                                <Avatar.Image src="https://media.discordapp.net/avatars/{user.id}/{user.avatar}" alt={user.global_name} />
-                                <Avatar.Fallback class="rounded-lg">
-                                    {user.global_name?.slice(0, 2).toUpperCase()}
-                                </Avatar.Fallback>
-                            </Avatar.Root>
-                            <div class="grid flex-1 text-left text-sm leading-tight">
-                                <span class="truncate font-medium">{user.global_name}</span>
-                                <span class="truncate text-xs opacity-50">{user.username}</span>
-                            </div>
-                        </div>
-                        <Tooltip.Provider>
-                            <Tooltip.Root>
-                                <Tooltip.Trigger>
-                                    <Button onclick={logout}>
-                                        <LucideLogOut class="size-5" />
-                                    </Button>
-                                </Tooltip.Trigger>
-                                <Tooltip.Content>
-                                    <p>Logout</p>
-                                </Tooltip.Content>
-                            </Tooltip.Root>
-                        </Tooltip.Provider>
-                    </div>
-                </Sidebar.MenuItem>
+                {#each data as item (item.title)}
+                    <Sidebar.MenuItem>
+                        <Sidebar.SidebarMenuButton isActive={item.url === page.url.pathname}>
+                            {#snippet child({ props })}
+                                <a href={item.url} {...props}>
+                                    <item.icon />
+                                    <span>{item.title}</span>
+                                </a>
+                            {/snippet}
+                        </Sidebar.SidebarMenuButton>
+                    </Sidebar.MenuItem>
+                {/each}
             </Sidebar.Menu>
-        </Sidebar.Footer>
-        <Sidebar.Rail />
-    </Sidebar.Root>
-    <Sidebar.Inset>
-        <header class="flex h-16 shrink-0 items-center gap-2 px-4">
-            <Sidebar.Trigger class="-ml-1" />
-            <Separator orientation="vertical" class="mr-2 data-[orientation=vertical]:h-4" />
-            <Breadcrumb.Root>
-                <Breadcrumb.List>
-                    <Breadcrumb.Item class="hidden md:block">
-                        <Breadcrumb.Link href="#">Admin Panel</Breadcrumb.Link>
-                    </Breadcrumb.Item>
-                    <Breadcrumb.Separator class="hidden md:block" />
-                    <Breadcrumb.Item>
-                        <Breadcrumb.Page>
-                            {#if page.url.pathname === "/admin"}
-                                Overview
-                            {:else}
-                                {page.url.pathname
-                                    .replace("/admin/", "") // Remove "/admin/"
-                                    .replace(/^\//, "") // Remove leading slash if any
-                                    .split("/") // Split by "/"
-                                    .map((segment) => segment.charAt(0).toUpperCase() + segment.slice(1)) // Title case each segment
-                                    .join(" / ")}
-                            {/if}
-                        </Breadcrumb.Page>
-                    </Breadcrumb.Item>
-                </Breadcrumb.List>
-            </Breadcrumb.Root>
-        </header>
-        {@render children()}
-    </Sidebar.Inset>
-</Sidebar.Provider>
+        </Sidebar.Group>
+    </Sidebar.Content>
+    <Sidebar.Footer>
+        <Sidebar.Menu>
+            <Sidebar.MenuItem>
+                <div class="flex items-center justify-between gap-2 px-1 py-1.5 text-left text-sm">
+                    <div class="flex items-center gap-2">
+                        <Avatar.Root class="size-8 rounded-lg">
+                            <Avatar.Image src="https://media.discordapp.net/avatars/{user.id}/{user.avatar}" alt={user.global_name} />
+                            <Avatar.Fallback class="rounded-lg">
+                                {user.global_name?.slice(0, 2).toUpperCase()}
+                            </Avatar.Fallback>
+                        </Avatar.Root>
+                        <div class="grid flex-1 text-left text-sm leading-tight">
+                            <span class="truncate font-medium">{user.global_name}</span>
+                            <span class="truncate text-xs opacity-50">{user.username}</span>
+                        </div>
+                    </div>
+                    <Tooltip.Provider>
+                        <Tooltip.Root>
+                            <Tooltip.Trigger>
+                                <Button onclick={logout}>
+                                    <LucideLogOut class="size-5" />
+                                </Button>
+                            </Tooltip.Trigger>
+                            <Tooltip.Content>
+                                <p>Logout</p>
+                            </Tooltip.Content>
+                        </Tooltip.Root>
+                    </Tooltip.Provider>
+                </div>
+            </Sidebar.MenuItem>
+        </Sidebar.Menu>
+    </Sidebar.Footer>
+    <Sidebar.Rail />
+</Sidebar.Root>
--- src/routes/(admin)/+layout.svelte
+++ src/routes/(admin)/+layout.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
     import { onNavigate } from "$app/navigation";
-    import AdminBar from "$lib/components/admin/Bar.svelte";
+    import { page } from "$app/state";
+    import Bar from "$lib/components/admin/Bar.svelte";
+    import * as Breadcrumb from "$lib/components/admin/ui/breadcrumb";
+    import { Separator } from "$lib/components/admin/ui/separator";
+    import * as Sidebar from "$lib/components/admin/ui/sidebar";
     import type { Snippet } from "svelte";
     import type { PageData } from "./admin/$types";
     import "./app.css";
 
     interface Props {
         data: PageData;
-        children?: Snippet;
+        children: Snippet;
     }
 
     let { data, children }: Props = $props();
@@ -28,6 +32,35 @@
     <title>JPA | Admin</title>
 </svelte:head>
 
-<AdminBar user={data.user}>
-    {@render children?.()}
-</AdminBar>
+<Sidebar.Provider>
+    <Bar user={data.user} />
+    <Sidebar.Inset>
+        <header class="flex h-16 shrink-0 items-center gap-2 px-4">
+            <Sidebar.Trigger class="-ml-1" />
+            <Separator orientation="vertical" class="mr-2 data-[orientation=vertical]:h-4" />
+            <Breadcrumb.Root>
+                <Breadcrumb.List>
+                    <Breadcrumb.Item class="hidden md:block">
+                        <Breadcrumb.Link href="#">Admin Panel</Breadcrumb.Link>
+                    </Breadcrumb.Item>
+                    <Breadcrumb.Separator class="hidden md:block" />
+                    <Breadcrumb.Item>
+                        <Breadcrumb.Page>
+                            {#if page.url.pathname === "/admin"}
+                                Overview
+                            {:else}
+                                {page.url.pathname
+                                    .replace("/admin/", "") // Remove "/admin/"
+                                    .replace(/^\//, "") // Remove leading slash if any
+                                    .split("/") // Split by "/"
+                                    .map((segment) => segment.charAt(0).toUpperCase() + segment.slice(1)) // Title case each segment
+                                    .join(" / ")}
+                            {/if}
+                        </Breadcrumb.Page>
+                    </Breadcrumb.Item>
+                </Breadcrumb.List>
+            </Breadcrumb.Root>
+        </header>
+        {@render children()}
+    </Sidebar.Inset>
+</Sidebar.Provider>
