35636648b5768a02a6b8b277018ecec215c9f25a
Ayush Sehrawat <69469790+AyushSehrawat@users.noreply.github.com>
2025-05-29 16:41:10 +0000

    feat: setup kysely-codegen

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,9 +1,9 @@
 import { Kysely, PostgresDialect } from 'kysely';
+import type { DB } from './types';
 import { Pool } from 'pg';
-import type { Database } from './types';
 import { DATABASE_URL } from '$env/static/private';
 
-export const kysely = new Kysely<Database>({
+export const db = new Kysely<DB>({
 	dialect: new PostgresDialect({
 		pool: new Pool({
 			connectionString: DATABASE_URL
--- src/lib/db/types.ts
+++
@@ -1,5 +0,0 @@
-import type { Database as SupabaseDatabase } from './database.types';
-import type { KyselifyDatabase } from 'kysely-supabase';
-
-// This converts the Supabase database type to a Kysely-compatible type.
-export type Database = KyselifyDatabase<SupabaseDatabase>;
