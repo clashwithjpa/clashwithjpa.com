44641a2d3fceb891eff792ca4b9b00b3742e0e5b
swayam25 <178068815+swayam25@users.noreply.github.com>
2026-03-23 10:54:13 +0530

    fix: update commitlint configuration to specify line length rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,8 @@
-export default { extends: ["@commitlint/config-conventional"] };
+export default {
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+        "header-max-length": [2, "always", 100],
+        "body-max-line-length": [2, "always", 2000],
+        "footer-max-line-length": [2, "always", 2000],
+    },
+};
