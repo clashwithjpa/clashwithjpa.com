0625da79a036bbd3489d31433a18c39604c13d85
swayam25 <178068815+swayam25@users.noreply.github.com>
2026-05-02 21:18:58 +0530

    fix: remove unused slash plugin styles from layout

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -3,7 +3,6 @@
     import "@cartamd/plugin-anchor/default.css";
     import "@cartamd/plugin-code/default.css";
     import "@cartamd/plugin-emoji/default.css";
-    import "@cartamd/plugin-slash/default.css";
     import "carta-md/default.css";
     import "./layout.css";
 
