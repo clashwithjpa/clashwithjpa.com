a4e40b23acdcee1079773e62bea09e91f73aa447
swayam25 <178068815+swayam25@users.noreply.github.com>
2026-08-03 16:28:20 +0530

    refactor: remove commitlint configuration and dependencies

--- commitlint.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    extends: ["@commitlint/config-conventional"],
-    rules: {
-        "header-max-length": [2, "always", 100],
-        "body-max-line-length": [2, "always", 2000],
-        "footer-max-line-length": [2, "always", 2000],
-    },
-};
